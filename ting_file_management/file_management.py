fc8d344c7b0ae6e0616ec44ccfe3ec59793771e1
Carolina Pedrosa <carolinasouza0@gmail.com>
2024-02-01 11:02:57 -0300

    feat: implements txt_importer function

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        print("Formato inválido", file=sys.stderr)
+    try:
+        with open(path_file, "r") as file:
+            file_lines = []
+            for line in file.readlines():
+                file_lines.append(line.strip("\n"))
+            return file_lines
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)

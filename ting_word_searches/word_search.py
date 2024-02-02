e27d7af20acf09a7d27ccd49a293fff20b7ecec9
Carolina Pedrosa <carolinasouza0@gmail.com>
2024-02-02 13:41:25 -0300

    feat: implements exists_word function in word_search module

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,22 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = list()
+
+    for i in range(len(instance)):
+        search_word_file = dict()
+        data_file = instance.search(i)
+        phrases = data_file["linhas_do_arquivo"]
+        occurrences = list()
+        for j, phrase in enumerate(phrases, start=1):
+            if word.lower() in phrase.lower():
+                occurrences.append({"linha": j})
+
+        if occurrences:
+            search_word_file["palavra"] = word
+            search_word_file["arquivo"] = data_file["nome_do_arquivo"]
+            search_word_file["ocorrencias"] = occurrences
+            result.append(search_word_file)
+
+    return result
 
 
 def search_by_word(word, instance):
